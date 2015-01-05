9007d6da621d2a6879e9549b758c871889ce7808
Tomohisa Kusano <siomiz@gmail.com>
2015-01-06 07:21:24 +0900

    PSK,USERNAME,PASSWORD can now be specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 set -e
 
+: ${PSK:='notasecret'}
+: ${USERNAME:=user$(cat /dev/urandom | tr -dc '0-9' | fold -w 4 | head -n 1)}
+
+printf '=%.0s' {1..24}
+echo
+echo ${USERNAME}
+
+if [[ $PASSWORD ]]
+then
+  echo '<use the password specified at -e PASSWORD>'
+else
+  PASSWORD=$(cat /dev/urandom | tr -dc '0-9' | fold -w 20 | head -n 1 | sed 's/.\{4\}/&./g;s/.$//;')
+  echo ${PASSWORD}
+fi  
+
+printf '=%.0s' {1..24}
+echo
+
 /opt/vpnserver start 2>&1 > /dev/null
 
 sleep 3
 
 # enable L2TP_IPsec
-/opt/vpncmd localhost /SERVER /CSV /CMD IPsecEnable /L2TP:yes /L2TPRAW:no /ETHERIP:no /PSK:notasecret /DEFAULTHUB:DEFAULT
+/opt/vpncmd localhost /SERVER /CSV /CMD IPsecEnable /L2TP:yes /L2TPRAW:yes /ETHERIP:no /PSK:${PSK} /DEFAULTHUB:DEFAULT
 
 # enable SecureNAT
 /opt/vpncmd localhost /SERVER /CSV /HUB:DEFAULT /CMD SecureNatEnable
 
 # add user
-UN=user$(cat /dev/urandom | tr -dc '0-9' | fold -w 4 | head -n 1)
-PW=$(cat /dev/urandom | tr -dc '0-9' | fold -w 20 | head -n 1 | sed 's/.\{4\}/&./g;s/.$//;')
-printf '=%.0s' {1..24}
-echo
-echo ${UN}
-echo ${PW}
-printf '=%.0s' {1..24}
-echo
-
-/opt/vpncmd localhost /SERVER /HUB:DEFAULT /CSV /CMD UserCreate ${UN} /GROUP:none /REALNAME:none /NOTE:none
-/opt/vpncmd localhost /SERVER /HUB:DEFAULT /CSV /CMD UserPasswordSet ${UN} /PASSWORD:${PW}
+/opt/vpncmd localhost /SERVER /HUB:DEFAULT /CSV /CMD UserCreate ${USERNAME} /GROUP:none /REALNAME:none /NOTE:none
+/opt/vpncmd localhost /SERVER /HUB:DEFAULT /CSV /CMD UserPasswordSet ${USERNAME} /PASSWORD:${PASSWORD}
 
 # set password for hub
 HPW=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 16 | head -n 1)
