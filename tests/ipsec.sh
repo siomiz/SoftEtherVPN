3a2a999de7bd743c61dea8e30111a8fc8de83142
Tomohisa Kusano <siomiz@gmail.com>
2017-11-06 14:39:42 -0800

    first attempt to have test cases

---
+++ tests/ipsec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+#!/bin/bash
+set -ex
+
+ipsec auto --up meraki
+
+echo "c meraki test test" > /var/run/xl2tpd/l2tp-control
+
+echo "d meraki" > /var/run/xl2tpd/l2tp-control
+
+ipsec down meraki
+
