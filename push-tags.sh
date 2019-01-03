78201ed4d2bd5363a699c7b2dd0e9361cb2f7472
Tomohisa Kusano <siomiz@gmail.com>
2019-01-02 17:44:09 -0800

    v4.28-9669-beta (#62)
    MAINT(all): v4.28-9669-beta

---
+++ push-tags.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+SE_VERSION="4.28"
+SE_REVISION="9669"
+
+BASE_TAGS="latest centos debian alpine ubuntu"
+
+for TAG in ${BASE_TAGS}; do
+  docker pull siomiz/softethervpn:${TAG}
+  VERSION_TAG=${SE_VERSION}-${TAG}
+  REVISION_TAG=${SE_REVISION}-${TAG}
+  docker tag siomiz/softethervpn:${TAG} siomiz/softethervpn:${VERSION_TAG%-latest}
+  docker tag siomiz/softethervpn:${TAG} siomiz/softethervpn:${REVISION_TAG%-latest}
+  docker push siomiz/softethervpn:${VERSION_TAG%-latest}
+  docker push siomiz/softethervpn:${REVISION_TAG%-latest}
+done
