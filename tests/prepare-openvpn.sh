3c24351302e5c69814d14a8ed9a9a495c893b6f5
Tomohisa Kusano <siomiz@gmail.com>
2019-10-25 11:59:31 -0700

    try ping thru tun0 for OpenVPN test

--- tests/prepare-openvpn.sh
+++ tests/prepare-openvpn.sh
@@ -9,3 +9,10 @@ apt-get install -y openvpn
 docker logs --follow vpntest-openvpn | grep -qe "[initial setup OK]"
 docker logs vpntest-openvpn > test.ovpn
 
+# adjust `remote`
+sed -i 's/^remote.*/remote 172.30.0.3 1194/' test.ovpn
+
+# make credentials file for `--auth-user-pass`
+echo test > userpass
+echo test >> userpass
+chmod 0400 userpass
--- tests/test-openvpn.sh
+++ tests/test-openvpn.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -ex
 
-openvpn --config test.ovpn --remote 172.30.0.3
+openvpn --config test.ovpn --auth-user-pass userpass --daemon --writepid openvpn.pid
+
+ping -c 3 -W 10 -I tun0 1.1.1.1
